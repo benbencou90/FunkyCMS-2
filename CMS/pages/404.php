21ef111289d7336035a819e76fd1106d6cf44311
benbencou90 <benben.cou90@gmail.com>
2012-09-12 14:58:54 +0300

    Update CMS/pages/404.php
    Correction de la page (C'est Benben )

--- CMS/pages/404.php
+++ CMS/pages/404.php
@@ -1,2 +1,5 @@
-<h2>Erreur 404</h2>
-<p>Page introuvable</p>
+<link rel="stylesheet" href="404.css" type="text/css" media="screen" />
+<center><h2>Erreur 404</h2></center> <br />
+<center><p>Page introuvable</p></center>
+<p>Nous allons vous redirigez vers la page d accueil dans 3 Secondes </p>
+<meta http-equiv="refresh" content="3;URL=http://funky-cms.net">
